b6d34a2ae691ddce342e9da131cd669e31ca1ab9
Zaiidmo <zaiidmoumnii@gmail.com>
2024-02-28 22:23:55 +0100

    UnitTests: Adding the AuthTest - login and register

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testLogin()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'user' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                ],
+                'authorization' => [
+                    'token',
+                    'type',
+                ],
+            ]);
+    }
+    public function testRegister()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'message' => 'User created successfully',
+                'user' => [
+                    'name' => 'John Doe',
+                    'email' => 'john@example.com',
+                ],
+            ]);
+    }
+    
+}

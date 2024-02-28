8b77bd638b0c4e5c7d3ba3074f8ed93f77b1929a
Zaiidmo <zaiidmoumnii@gmail.com>
2024-02-28 22:58:08 +0100

    UnitTests: TasksTest - Create a Task

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -13,7 +13,7 @@ class TasksTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_returns_tasks_for_authenticated_user()
+    public function displayMyTasks()
     {
         // Create a user and authenticate
         $user = User::factory()->create();
@@ -30,7 +30,31 @@ class TasksTest extends TestCase
         // Assert the response
         $response->assertOk()->assertJson($tasks->toArray());
     }
+    /** @test */
+    public function createATask()
+    {
+        // Create a user and authenticate
+        $user = User::factory()->create();
+        $this->actingAs($user, 'api');
 
+        // Define task data
+        $taskData = [
+            'title' => 'Test Task',
+            'description' => 'This is a test task',
+        ];
+
+        // Invoke the store method
+        $response = $this->postJson('/api/tasks', $taskData);
+
+        // Assert the response
+        $response->assertStatus(200)->assertJson([
+            'message' => 'Task created successfully',
+            'task' => $taskData,
+        ]);
+
+        // Assert that the task was created in the database
+        $this->assertDatabaseHas('tasks', $taskData);
+    }
     /** @test */
     
 }
